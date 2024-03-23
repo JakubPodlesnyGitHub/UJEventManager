f8fe0eaec4f57901f6b859b1df97ac707f554a49
Jakub Podlesny <jakub.podlesny2109@gmail.com>
2024-03-23 12:17:14 +0100

    Created Base Configuration For Repositories

---
+++ Backend/Shop/Shop.Infrastructure/Configuration/InfrastructureConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Infrastructure.Configuration
+{
+    public static class InfrastructureConfiguration
+    {
+        public static IServiceCollection AddInfrastructureConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRepositoriesConfiguration(configuration);
+            return services;
+        }
+    }
+}
---
+++ Backend/Shop/Shop.Infrastructure/Configuration/RepositoriesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Infrastructure.DbContexts;
+using Shop.Infrastructure.Repositories;
+using Shop.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Infrastructure.Configuration
+{
+    internal static class RepositoriesConfiguration
+    {
+        public static IServiceCollection AddRepositoriesConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
