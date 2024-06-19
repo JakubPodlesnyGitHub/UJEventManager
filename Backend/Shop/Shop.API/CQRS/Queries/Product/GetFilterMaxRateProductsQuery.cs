eea43974e5934de386bcc95017d652c821ed9b12
Jakub Podlesny <jakub.podlesny2109@gmail.com>
2024-06-19 19:57:40 +0200

    sort fix and added two filter endpoints

---
+++ Backend/Shop/Shop.API/CQRS/Queries/Product/GetFilterMaxRateProductsQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Shop.Shared.Dtos.Response;
+
+namespace Shop.API.CQRS.Queries.Product
+{
+    public class GetFilterMaxRateProductsQuery : IRequest<IList<ProductDTO>>
+    {
+
+        public double Max { get; set; }
+
+        public GetFilterMaxRateProductsQuery(double max)
+        {
+            Max = max;
+        }
+    }
+}
