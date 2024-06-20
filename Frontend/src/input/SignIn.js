0a4f1576b38b15cdc21dd9f7a3bca3670d3e234a
Kristofer <reczko1@gmail.com>
2024-06-21 00:28:40 +0200

    Added endpoints and logic to components

--- Frontend/src/input/SignIn.js
+++ Frontend/src/input/SignIn.js
@@ -1,19 +1,55 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState, useContext } from "react";
+import { Button, Form, Container, Alert } from "react-bootstrap";
+
+
 
 export default function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [variant, setVariant] = useState('');
+
+  const handleLogin = async () => {
+    const response = await fetch("http://localhost:5164/api/Auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: {email},
+            password: {password}
+        }),
+    });
+    // setMessage('Login successful!');
+    // setVariant('success');
+    // if (response.ok) {
+    //     // const updatedData = await response.json();
+    //     setMessage('Login successful!');
+    //     setVariant('success');
+    //     // alert('Login successful!');
+    // } else {
+    //     // console.error("Failed to create new product");
+    //     setMessage('Login failed!');
+    //     setVariant('danger');
+    //     // alert('Login failed!');
+    // }
+};
+
+  
   return (
-    <>
+    <Container>
       <div className="frame_spacing">
+        {/* {message && <Alert variant={variant}>{message}</Alert>} */}
         <h2 className="form_title">Sign in</h2>
-        <Form className="between_fields_spacing">
+        <Form className="between_fields_spacing" onSubmit={handleLogin}>
           <Form.Group className="between_fields_spacing" controlId="email">
             <Form.Label className="text-center">Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
           </Form.Group>
           <Form.Group className="between_fields_spacing" controlId="password">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
           </Form.Group>
           <div className="centered">
             <Button variant="primary" size="lg" type="submit">
@@ -22,6 +58,6 @@ export default function SignIn() {
           </div>
         </Form>
       </div>
-    </>
+    </Container>
   );
 }
--- Frontend/src/input/SignUp.js
+++ Frontend/src/input/SignUp.js
@@ -1,22 +1,76 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Button, Form } from "react-bootstrap";
 
 export default function SignUp() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+  // const { login } = useContext(AuthContext);
+
+  const handleRegister = async () => {
+    const response = await fetch("http://localhost:5164/api/Auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            firstName: {firstName},
+            lastName: {lastName},
+            birthDate: "2024-06-20T20:24:21.747Z", //to simplify use string here
+            picture: "string", //to simplify use string here
+            email: {email},
+            phoneNumber: {phoneNumber},
+            password: {password},
+            isAdmin: false,
+        }),
+    });
+    if (response.ok) {
+        // const updatedData = await response.json();
+        alert('Registration successful!');
+    } else {
+        // console.error("Failed to create new product");
+        alert('Registration failed');
+    }
+};
+
   return (
     <div className="frame_spacing">
       <h2 className="form_title">Sign up</h2>
-      <Form className="between_fields_spacing">
-        <Form.Group className="between_fields_spacing" controlId="name">
-          <Form.Label className="text-center">Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Your name" />
+      <Form className="between_fields_spacing" onSubmit={handleRegister}>
+        <Form.Group className="between_fields_spacing" controlId="firstName">
+          <Form.Label className="text-center">First Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Your first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required/>
+        </Form.Group>
+        <Form.Group className="between_fields_spacing" controlId="lastName">
+          <Form.Label className="text-center">Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Your last name" value={lastName} onChange={(e) => setLastName(e.target.value)} required/>
         </Form.Group>
         <Form.Group className="between_fields_spacing" controlId="email">
           <Form.Label className="text-center">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        </Form.Group>
+        <Form.Group className="between_fields_spacing" controlId="phoneNumber">
+          <Form.Label className="text-center">Phone number</Form.Label>
+          <Form.Control type="text" placeholder="Enter phone number e.g. 777-777-777" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required/>
         </Form.Group>
         <Form.Group className="between_fields_spacing" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        </Form.Group>
+        <Form.Group controlId="formRole">
+          <Form.Label>Role</Form.Label>
+          <Form.Control
+            as="select"
+            value={isAdmin ? 'admin' : 'user'}
+            onChange={(e) => setIsAdmin(e.target.value === 'admin')}
+          >
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </Form.Control>
         </Form.Group>
         <div className="centered">
           <Button variant="primary" size="lg" type="submit">
